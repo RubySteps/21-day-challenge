fcbd9b24f20573c3ac26a9f48e6de48bae05f6bf
Steve Tooke <steve@boxjump.co.uk>
2015-04-15 09:47:46 +0100

    First step making tests executable things

--- 2_adventures/001/tooky/01/tiny_test.rb
+++ 2_adventures/001/tooky/01/tiny_test.rb
@@ -4,14 +4,18 @@ def assert(condition)
   raise AssertionFailed unless condition
 end
 
-assert(1 + 1 == 2)
+assert_does_not_raise_for_true_condition = -> () {
+  assert(1 + 1 == 2)
+}.call
 
-begin
-  assert(1 + 1 != 2)
-rescue AssertionFailed
-  raised = true
-else
-  fail
-end
+assert_raises_for_false_condition = -> () {
+  begin
+    assert(1 + 1 != 2)
+  rescue AssertionFailed
+    raised = true
+  else
+    fail
+  end
+}.call
 
 puts "Success!"
