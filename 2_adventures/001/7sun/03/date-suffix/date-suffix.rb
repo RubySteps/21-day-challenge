45037dbc5b8db59134b1a7d7edd79ee62bf00584
7sun <pakrouse@gmail.com>
2015-04-15 20:32:46 -0700

    light refactoring

--- 2_adventures/001/7sun/03/date-suffix/date-suffix.rb
+++ 2_adventures/001/7sun/03/date-suffix/date-suffix.rb
@@ -1,16 +1,16 @@
 def add_suffix(num)
-  last = num.to_s.split("").last
-  second_to_last = num.to_s.split("").reverse[1]
-  if last == "1"
+  num_array = num.to_s.split("")
+  last = num_array.last
+  second_to_last = num_array.reverse[1]
+  if second_to_last == "1"
+    suffix = "th"
+  elsif last == "1"
     suffix = "st"
   elsif last == "2"
     suffix = "nd"
   elsif last == "3"
     suffix = "rd"
   else
-    suffix = "th"
-  end
-  if second_to_last == "1"
     suffix = "th"
   end
   num.to_s + suffix
