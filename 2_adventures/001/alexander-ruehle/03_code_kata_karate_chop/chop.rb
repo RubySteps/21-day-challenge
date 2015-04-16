733f8a71a90d0e1bb7ff0dd16d5665032d9375d2
Alexander Rühle <alexander.ruehle@googlemail.com>
2015-04-16 11:32:19 +0200

    adds karate chop method, tests are valid

--- 2_adventures/001/alexander-ruehle/03_code_kata_karate_chop/chop.rb
+++ 2_adventures/001/alexander-ruehle/03_code_kata_karate_chop/chop.rb
@@ -1,5 +1,23 @@
 class Chop
-	def self.chop(search, array)
 
+	def self.chop(search, array)
+		self.binary_search(search, array, 0, array.length - 1)
+	end
+
+
+	private
+
+	def self.binary_search(search, array, bottom, top)
+		return -1 if top < bottom
+
+		middle = (bottom + top) / 2
+
+		if array[middle] > search
+			self.binary_search(search, array, bottom, middle - 1)
+		elsif array[middle] < search
+			self.binary_search(search, array, middle + 1, top)
+		else
+			return middle
+		end
 	end
 end
--- 2_adventures/001/alexander-ruehle/03_code_kata_karate_chop/chop_test.rb
+++ 2_adventures/001/alexander-ruehle/03_code_kata_karate_chop/chop_test.rb
@@ -7,7 +7,7 @@ class ChopTest < Test::Unit::TestCase
     assert_equal(-1, Chop.chop(3, []))
     assert_equal(-1, Chop.chop(3, [1]))
     assert_equal(0,  Chop.chop(1, [1]))
-
+    #
     assert_equal(0,  Chop.chop(1, [1, 3, 5]))
     assert_equal(1,  Chop.chop(3, [1, 3, 5]))
     assert_equal(2,  Chop.chop(5, [1, 3, 5]))
@@ -15,7 +15,7 @@ class ChopTest < Test::Unit::TestCase
     assert_equal(-1, Chop.chop(2, [1, 3, 5]))
     assert_equal(-1, Chop.chop(4, [1, 3, 5]))
     assert_equal(-1, Chop.chop(6, [1, 3, 5]))
-
+    #
     assert_equal(0,  Chop.chop(1, [1, 3, 5, 7]))
     assert_equal(1,  Chop.chop(3, [1, 3, 5, 7]))
     assert_equal(2,  Chop.chop(5, [1, 3, 5, 7]))
