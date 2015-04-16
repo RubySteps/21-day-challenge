4f1e5040dd08efe8a03e53b2700f8b6da203de7c
Thomas Carney <tommycarney@gmail.com>
2015-04-16 14:35:24 +0200

    look up info on GB post codes

---
+++ 2_adventures/001/tommycarney/03/postcode.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# postcode.rb
+# Works with an API without authenication
+require 'open-uri'
+require 'json'
+class Postcode
+	attr_accessor :enter_postcode
+	attr_accessor :json_request
+	attr_accessor :postcode
+	attr_accessor :response
+	
+	def intialize
+	end
+
+	def enter_postcode
+		puts "Please enter a postcode"
+		post_code = gets.chomp
+		if (post_code.length == 6) || (post_code.length == 7)
+			@postcode = post_code
+			display_postcode
+		else
+			puts "Not a valid postcode \nPostcodes must be 6 or 7 digits long"
+			enter_postcode
+		end
+	end
+
+	def json_request
+		result = JSON.parse(open("https://api.postcodes.io/postcodes/#{@postcode}").read)
+		return result
+	end
+
+	def display_postcode
+		results = json_request
+		return "There was an error" unless (results["status"] == 200)
+		info = results["result"]
+		puts "It's situated at #{info["longitude"]} : #{info["latitude"]}"
+		puts "The region is #{info["region"]}"
+	end
+
+end
+
+if __FILE__ == $0
+
+	postcode = Postcode.new
+	postcode.enter_postcode
+
+end
