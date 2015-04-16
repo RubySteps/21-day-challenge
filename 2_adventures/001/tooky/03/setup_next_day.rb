2117f0e48d506a204577170fa73e66932bbd8a49
Steve Tooke <steve@boxjump.co.uk>
2015-04-16 15:08:09 +0100

    Tweaks for next day script
    * Truncate README rather than remove
    * commit start to git

--- 2_adventures/001/tooky/03/setup_next_day.rb
+++ 2_adventures/001/tooky/03/setup_next_day.rb
@@ -3,6 +3,11 @@ require 'fileutils'
 
 current_directory = File.expand_path(File.dirname(__FILE__))
 next_directory    = current_directory.succ
+day_number        = File.basename(next_directory)
 
 FileUtils.cp_r current_directory, next_directory
-FileUtils.rm File.join(next_directory, "README.md")
+File.truncate File.join(next_directory, "README.md")
+
+`git reset && git add #{next_directory} && git commit -m "Start day #{day_number}"`
+
+puts "Done!"
