5be5876720f8921e6fd23fec69295315d0145f3a
7sun <pakrouse@gmail.com>
2015-04-16 23:14:24 -0700

    adds readme to day 3 and adds day 4 dir with readme

--- 2_adventures/001/7sun/03/date-suffix/date-suffix.rb
+++ 2_adventures/001/7sun/03/date-suffix/date-suffix.rb
@@ -1,17 +1,19 @@
 def add_suffix(num)
   num_array = num.to_s.split("")
-  last = num_array.last
-  second_to_last = num_array.reverse[1]
+  last, second_to_last = num_array.last, num_array[-2]
   if second_to_last == "1"
     suffix = "th"
-  elsif last == "1"
-    suffix = "st"
-  elsif last == "2"
-    suffix = "nd"
-  elsif last == "3"
-    suffix = "rd"
   else
-    suffix = "th"
+    case last
+      when "1"
+        suffix = "st"
+      when "2"
+        suffix = "nd"
+      when "3"
+        suffix = "rd"
+      else
+        suffix = "th"
+    end
   end
   num.to_s + suffix
 end
---
+++ 2_adventures/001/7sun/04/Readme.md
@@ -0,0 +1,1 @@
+This is the readme for the ruby challenge, Day 4
