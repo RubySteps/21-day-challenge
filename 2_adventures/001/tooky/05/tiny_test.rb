2d55cf8b127e4d70cf4eb8380b691e81b8ad5925
Steve Tooke <steve@boxjump.co.uk>
2015-04-18 11:40:01 +0100

    Added a rudimentary sketch / spike for reporting errors.
    Would be better if I could record the names of tests...

--- 2_adventures/001/tooky/05/tiny_test.rb
+++ 2_adventures/001/tooky/05/tiny_test.rb
@@ -114,7 +114,17 @@ result = TinyTest.run_all(
   report_test_failure_details
 )
 
-puts "#{result.run_count} ran, #{result.passed_count} passed, #{result.failed_count} failed"
+if result.failures.any?
+  puts "Tests Failed:"
+  puts
 
+  result.failures.each do |f|
+    puts "  #{f.exception} - #{f.exception.message}"
+    puts f.exception.backtrace.map { |s| "    #{s}" }
+    puts
+  end
+end
+
+puts "#{result.run_count} ran, #{result.passed_count} passed, #{result.failed_count} failed"
 
 puts "Success!" if result.run_count == result.passed_count
