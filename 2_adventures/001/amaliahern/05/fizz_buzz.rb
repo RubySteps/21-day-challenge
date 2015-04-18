1bd26d5ad628852d56dd7e4ffe74d6e9788d8b54
Amalia Hernandez Garcia <amalia@Amalias-MacBook-Pro.local>
2015-04-18 21:50:21 +0200

    DAY 5: new version of fizzbuzz kata thanks to Victor Goff for his help.

---
+++ 2_adventures/001/amaliahern/05/fizz_buzz.rb
@@ -0,0 +1,14 @@
+DIVISIBLE_BY_3 = "fizz"
+DIVISIBLE_BY_5 = "buzz"
+
+def fizzbuzz(number)
+  result = number
+  if(number % 3 == 0 and number % 5 == 0)
+    result = DIVISIBLE_BY_3 + DIVISIBLE_BY_5  
+  elsif(number % 3 == 0)
+    result = DIVISIBLE_BY_3 
+  elsif(number % 5 == 0)
+    result = DIVISIBLE_BY_5 
+  end
+  result
+end
