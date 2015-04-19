6670080fa2e009b2635cebd4da617808f2e23830
sbartek <bartekskorulski@gmail.com>
2015-04-19 15:04:26 +0200

    refactors and add spec for creating a questionnare from hash

--- 2_adventures/001/sbartek/06/questionnaires.rb
+++ 2_adventures/001/sbartek/06/questionnaires.rb
@@ -5,9 +5,9 @@ class Questionnaire
   attr_accessor :title
   attr_accessor :questions
 
-  def initialize(title="")
-    @title = title
-    @quesitons = []
+  def initialize(arg_hash={})
+    @title = arg_hash[:title]
+    @questions = arg_hash[:questions]
   end
 end
 
@@ -21,19 +21,40 @@ class Question
 
 end
 
+def create_questionnaire_from_hash(arg_hash)
+  
+end
+
 describe Questionnaire do
   
-  before do
-    @questionnaire = Questionnaire.new
-  end
+  describe '#initialize' do
+    describe 'with no args' do
+      before do
+        @questionnaire = Questionnaire.new
+      end
 
-  it "must respond to title" do
-    @questionnaire.must_respond_to(:title)
-  end
+      it "must respond to title" do
+        @questionnaire.must_respond_to(:title)
+      end
+      
+      it "must respond to questions" do 
+        @questionnaire.must_respond_to(:questions)
+      end
+    end
 
-  it "must respond to questions" do 
-    @questionnaire.must_respond_to(:questions)
-  end
+    it  'must initialize with title' do
+      @title = "Some title"
+      @questionnaire = Questionnaire.new title: @title
+      @questionnaire.title.must_equal @title
+    end
+
+    it 'must initialize with questions' do
+      @questions = ['some question', 'another']
+      @questionnaire = Questionnaire.new questions: @questions
+      @questionnaire.questions.must_equal @questions
+    end
+
+  end  
 end
 
 describe Question do
@@ -54,3 +75,9 @@ describe Question do
   end
 end
 
+describe create_questionnaire_from_hash do
+  it 'must create questionnaire' do
+    @questionnaire = create_questionnaire_from_hash({})
+    @questionnaire.must_be_instance_of(Questionnaire)
+  end
+end
