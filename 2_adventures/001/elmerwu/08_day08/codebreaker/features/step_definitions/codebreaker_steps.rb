12e65e32b273789254c6bffebdb48ab3d625f9ec
Elmer Wu <elmerwu@gmail.com>
2015-04-21 07:31:12 -0700

    update with Output Class, When and Then steps

--- 2_adventures/001/elmerwu/08_day08/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ 2_adventures/001/elmerwu/08_day08/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,6 +1,26 @@
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||= Output.new
+end
+
 Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-  Codebreaker::Game.new.start
+  game = Codebreaker::Game.new(output)
+  game.start
 end
+
+Then /^I should see "([^"]*)$/ do |message|
+  output.messages.should include(message)
+end
+
