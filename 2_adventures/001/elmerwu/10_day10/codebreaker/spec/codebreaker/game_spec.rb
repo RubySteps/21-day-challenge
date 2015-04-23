2342c7c7ae293cbc01eb6295242f9f3a025b6bf0
Elmer Wu <elmerwu@gmail.com>
2015-04-22 23:21:10 -0700

    update game_spec to move object declaration to before block

--- 2_adventures/001/elmerwu/10_day10/codebreaker/spec/codebreaker/game_spec.rb
+++ 2_adventures/001/elmerwu/10_day10/codebreaker/spec/codebreaker/game_spec.rb
@@ -3,23 +3,19 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
+      before(:each) do
+        @out = double('out').as_null_object
+        @game = Game.new(@out)
+      end
 
       it "sends a welcome message" do
-        out = double('out').as_null_object
-        game = Game.new(out)
-
-        expect(out).to receive(:puts).with("Welcome to Codebreaker!")
-
-        game.start
+        expect(@out).to receive(:puts).with("Welcome to Codebreaker!")
+        @game.start
       end
 
       it "prompts for the first guess" do
-        out = double('out').as_null_object
-        game = Game.new(out)
-
-        expect(out).to receive(:puts).with("Enter guess:")
-
-        game.start
+        expect(@out).to receive(:puts).with("Enter guess:")
+        @game.start
       end
     end
   end
