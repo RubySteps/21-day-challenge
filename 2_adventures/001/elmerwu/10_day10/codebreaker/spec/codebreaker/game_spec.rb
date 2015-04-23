2203ce2e878fe31023e91cfc9f78f4c30b5ba5c8
Elmer Wu <elmerwu@gmail.com>
2015-04-22 23:23:35 -0700

    update game_spec to use let method

--- 2_adventures/001/elmerwu/10_day10/codebreaker/spec/codebreaker/game_spec.rb
+++ 2_adventures/001/elmerwu/10_day10/codebreaker/spec/codebreaker/game_spec.rb
@@ -3,19 +3,17 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
-      before(:each) do
-        @out = double('out').as_null_object
-        @game = Game.new(@out)
-      end
+        let(:out) { double('out').as_null_object }
+        let(:game) { Game.new(out) }
 
       it "sends a welcome message" do
-        expect(@out).to receive(:puts).with("Welcome to Codebreaker!")
-        @game.start
+        expect(out).to receive(:puts).with("Welcome to Codebreaker!")
+        game.start
       end
 
       it "prompts for the first guess" do
-        expect(@out).to receive(:puts).with("Enter guess:")
-        @game.start
+        expect(out).to receive(:puts).with("Enter guess:")
+        game.start
       end
     end
   end
