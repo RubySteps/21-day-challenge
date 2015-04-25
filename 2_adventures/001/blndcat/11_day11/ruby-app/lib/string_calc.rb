0b3eb672f0ff3a08b73ad89bb1ffb275fe897d19
Ian Cheung <ianc@starconcepts.co.uk>
2015-04-25 22:46:29 +0900

    working on single number

--- 2_adventures/001/blndcat/11_day11/ruby-app/lib/string_calc.rb
+++ 2_adventures/001/blndcat/11_day11/ruby-app/lib/string_calc.rb
@@ -1,6 +1,13 @@
+class Object
+  def is_number?
+    self.to_f.to_s == self.to_s || self.to_i.to_s == self.to_s
+  end
+end
+
 class StringCalc
 
   def add(input_string)
     return 0 if input_string.empty?
+    return input_string if input_string.is_number?
   end
 end
--- 2_adventures/001/blndcat/11_day11/ruby-app/spec/string_calc_spec.rb
+++ 2_adventures/001/blndcat/11_day11/ruby-app/spec/string_calc_spec.rb
@@ -10,10 +10,23 @@ describe StringCalc do
     end
 
     it "does not return 0 if string is not empty" do
+      pending
       result = StringCalc.new.add("abc")
 
       expect(result).not_to eq 0
 
+    end
+
+    it "returns number if single number" do
+      result = StringCalc.new.add("1")
+
+      expect(result).to eq 1
+    end
+
+    it "returns number if single number even if 0" do
+      result = StringCalc.new.add("0")
+
+      expect(result).to eq 0
     end
   end
 end
