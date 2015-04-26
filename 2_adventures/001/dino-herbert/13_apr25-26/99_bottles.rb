33cc68e6e8a43298024f5e51173f964b2ea85f55
Dino-Herbert <dherbert5764@gmail.com>
2015-04-26 09:05:14 -0400

    Day13_apr25-26

---
+++ 2_adventures/001/dino-herbert/13_apr25-26/99_bottles.rb
@@ -0,0 +1,6 @@
+99.downto 1 do |b|
+  puts "#{b} bottles of beer on the wall. #{b} bottles of beer.  Take one down pass it around. #{b -1} bottles of beer on the wall."
+  #need to add change to one bottle vs bottles
+end
+
+puts "We're officially intoxicated."
