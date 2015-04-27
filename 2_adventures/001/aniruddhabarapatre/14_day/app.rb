86fd5296e0c5066b6f1bfac8bd1e6d5236014a70
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-04-26 21:55:37 -0400

    Add to database from web app
    Create new to do by modifying post and saving to list.

--- 2_adventures/001/aniruddhabarapatre/14_day/app.rb
+++ 2_adventures/001/aniruddhabarapatre/14_day/app.rb
@@ -4,3 +4,14 @@ get '/todos' do
   @todos = ToDo.all
   erb :index, layout: :bootstrap
 end
+
+get '/todos/new' do
+  @todos = ToDo.all
+  erb :new, layout: :bootstrap
+end
+
+post '/todos' do
+  @post = ToDo.new(params[:todo])
+  @post.save
+  redirect '/todos'
+end
