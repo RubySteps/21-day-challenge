61832bb2dae9888202739dacf75f70feb2b1561c
Ian Cheung <ianc@starconcepts.co.uk>
2015-04-27 15:29:32 +0900

    two numbers working, ready to refactor

--- 2_adventures/001/blndcat/14_day14/ruby-app/lib/string_calc.rb
+++ 2_adventures/001/blndcat/14_day14/ruby-app/lib/string_calc.rb
@@ -5,5 +5,10 @@ class StringCalc
     if input_string.to_i.to_s == input_string.to_s
       return input_string.to_i
     end
+    if input_string.include? ','
+      number1, number2 = input_string.split(',')
+      result = number1.to_i + number2.to_i
+      return result
+    end
   end
 end
