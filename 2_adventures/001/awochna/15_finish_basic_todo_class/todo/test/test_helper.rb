c26b78a7ffa4fa8dd4648c1dbcfd81e8be3d62df
Alex Wochna <awochna@email.arizona.edu>
2015-04-27 11:06:03 -0700

    Add code from previous day

---
+++ 2_adventures/001/awochna/15_finish_basic_todo_class/todo/test/test_helper.rb
@@ -0,0 +1,13 @@
+require 'todo'
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/pride'
+
+# Add test libraries you want to use here, e.g. mocha
+
+class MiniTest::Test
+
+  # Add global extensions to the test case class here
+  
+end
