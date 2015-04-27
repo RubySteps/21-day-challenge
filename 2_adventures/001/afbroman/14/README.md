949c60a7b20ddac31e868718006fdec57004558b
Andrew Broman <broman@acm.org>
2015-04-26 19:42:26 -0500

    afbroman day 14 challenge
    A first pass at a plot of the data output with yesterday’s code

---
+++ 2_adventures/001/afbroman/14/README.md
@@ -0,0 +1,8 @@
+## RubySteps 21-Day Challenge: Day 14
+
+From the code I was working on yesterday (or, more precisely, from the CSV output from that code), I have made a first pass at a static D3.js plot.
+
+There are obviously some problems with it:
+
+1. The lines are meant to be the same colors as their labels. I’m not sure yet why this isn’t happening.
+2. Two labels are completely overlapping. This will probably have to be a special-case fix.
---
+++ 2_adventures/001/afbroman/14/opioid_data.rb
@@ -0,0 +1,79 @@
+require "csv"
+require_relative 'database_info.rb'
+
+class OpioidData
+
+  MULTIPLIERS = { "Fentanyl" => 83.33,
+                  "Hydromorphone" => 5,
+                  "Methadone" => 4,
+                  "Morphine" => 1,
+                  "Oxycodone" => 1.33,
+                  "Pethidine" => 0.25 }
+
+  DB = DatabaseInfo.new(env: "development").dbconnect
+
+  def fetch(start_year="1980", end_year="2012")
+    drugs_array = [2,3,4,5,6,7]
+
+    CSV.open("opioids.csv", "wb") do |csv|
+      write_line(csv,
+                 %w[Year] + get_drug_display_names(drugs_array) + ["Total ME"])
+      (start_year..end_year).each do |year|
+        curr_line = [year]
+        global_pop = get_global_population(year)
+        drugs_array.each do |drug_id|
+          temp = DB[:consumptions].where(drug_id: drug_id, year: year)
+          temp_kg = temp.sum(:kg) * MULTIPLIERS[get_drug_name_from_id(drug_id)]
+          curr_line << temp_kg * 1_000_000 / global_pop
+        end
+        me_kg = calculate_global_kg(year, "Morphine Equivalence")
+        curr_line << me_kg * 1_000_000 / global_pop
+        write_line(csv, curr_line)
+      end
+    end
+  end
+
+
+  private
+
+  def write_line(csv, arry)
+    csv << arry
+  end
+
+
+  def get_drug_name_from_id(drug_id)
+    DB[:drugs].where(id: drug_id).get(:name)
+  end
+
+  def get_drug_display_names(id_array)
+    id_array.map do |id|
+      get_drug_name_from_id(id)
+    end
+  end
+
+  def get_global_population(year)
+    populations = get_population_data
+    Integer(populations[year]["global"])
+  end
+
+  def calculate_global_kg(year, drug_name="morphine equivalence")
+    DB[:consumptions].join(:drugs, id: :drug_id).where(drugs__name: drug_name, year: year).sum(:kg)
+  end
+
+  def get_population_data
+    who_file = "who_populations.csv"
+    populations = CSV.read(who_file)
+    pop_hash = Hash.new
+    fields = populations[0]
+
+    fields_lines = fields[1..-1].zip((1..7).to_a)
+
+    populations.drop(1).each do |line|
+      curr = pop_hash[line[0]] = Hash.new
+      fields_lines.each do |field, line_num|
+        curr[field] = line[line_num]
+      end
+    end
+    pop_hash
+  end
+end
