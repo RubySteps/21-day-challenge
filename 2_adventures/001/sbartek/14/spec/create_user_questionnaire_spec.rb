28c3d1aee7d56a4f206a55f14dfc02575bb8daef
sbartek <bartekskorulski@gmail.com>
2015-04-27 16:38:26 +0200

    adds specs for creating user answers to questions

---
+++ 2_adventures/001/sbartek/14/spec/create_user_questionnaire_spec.rb
@@ -0,0 +1,34 @@
+require_relative 'helper'
+require './create_questionnaire'
+require './create_user_questionnaire'
+
+describe 'create_user_questionnaire_from_hash' do
+  before do
+    @sentence1 = "Whats your name?"
+    @sentence2 = "Whats your pet's name?"
+    @questions = [{sentence: @sentence1}, {sentence: @sentence2}]
+    @questionnaire = create_questionnaire_from_hash(
+        {questions: @questions})
+    @user = "Tom"
+    @questions_answers = [{sentence: @sentence1, answer: "Tomy"}, 
+                          {sentence: @sentence2, answer: "Yorky"}]
+    @user_questionnaire = create_user_questionnaire_from_hash(
+        {user: @user,
+         questionnaire: @questionnaire, 
+         questions_answers: @questions_answers})
+  end
+
+  it 'must create UserQuestionnaire'  do
+    @user_questionnaire.must_be_instance_of(UserQuestionnaire)
+  end
+
+  it 'must have a user assigned' do
+    @user_questionnaire.user.must_equal(@user)
+  end
+
+  it 'must have answers to questions' do
+    @questionnaire.questions.each do |question| 
+      @user_questionnaire.for_question(question).answer.wont_be_nil
+    end
+  end
+end
