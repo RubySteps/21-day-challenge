1e07192ab847fceee39bcd1ab291d051ea783df0
Jon Kern <jonkern@comcast.net>
2015-04-28 08:44:39 -0400

    Day 15-2: Got back to 100% code coverage on rspec. Some wip cukes still

---
+++ 2_adventures/001/jonkernpa/15_day2/lib/turmite.rb
@@ -0,0 +1,65 @@
+class Turmite
+
+  NORTH = 0
+  EAST = 1
+  SOUTH = 2
+  WEST = 4
+
+  attr_accessor :state, :square, :location, :direction, :color
+
+  def initialize(x: 0, y: 0, square: 0, direction: NORTH, state: 0, color: 0)
+    @direction = direction
+    @state = state
+    @color = color
+    @square = square
+    @location = location = [x, y]
+  end
+
+  #             N (+y)
+  #             ^
+  #             |
+  # (-x) W <----+----> E (+x)
+  #             |
+  #             v
+  #             S (-y)
+  def move
+    dx = dy = 0
+    case @direction
+      when Turmite::NORTH
+        dx = 0
+        dy = +1
+      when Turmite::SOUTH
+        dx = 0
+        dy = -1
+      when Turmite::EAST
+        dx = 1
+        dy = 0
+      when Turmite::WEST
+        dx = -1
+        dy = 0
+    end
+    new_x = @location.first + dx
+    new_y = @location.last + dy
+    @location = [new_x, new_y]
+  end
+
+  def turn
+    if state == 0 && (color == 0 || color == 1)
+      # turn right
+      case @direction
+        when Turmite::NORTH
+          new_direction = Turmite::EAST
+        when Turmite::SOUTH
+          new_direction = Turmite::WEST
+        when Turmite::EAST
+          new_direction = Turmite::SOUTH
+        when Turmite::WEST
+          new_direction = Turmite::NORTH
+      end
+      @direction = new_direction
+    else
+
+    end
+  end
+
+end
