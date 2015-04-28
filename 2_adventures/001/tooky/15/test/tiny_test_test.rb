9b74ff45f32e2fc5a8303156cb0ccd4a9c727708
Steve Tooke <steve@boxjump.co.uk>
2015-04-28 10:29:15 +0100

    Extract assert equal tests

--- 2_adventures/001/tooky/15/test/tiny_test_test.rb
+++ 2_adventures/001/tooky/15/test/tiny_test_test.rb
@@ -1,45 +1,7 @@
 require 'tiny_test/autorun'
 
 
-TinyTest.specify "assert_equal" do
-  must "assert_equal_does_not_raise_when_values_are_equal" do
-    expected = 2
-    actual   = 1 + 1
-    assert_equal expected, actual
-  end
 
-  must "assert_equal_raises_assertion_failed_when_not_equal" do
-    expected = 2
-    actual   = 1 + 2
-    begin
-      assert_equal expected, actual
-    rescue TinyTest::Assertions::Failed
-    else
-      fail "Should have raised"
-    end
-  end
-
-  must "default_message_for_assert_equal" do
-    expected = 2
-    actual   = 1 + 2
-    begin
-      assert_equal expected, actual
-    rescue TinyTest::Assertions::Failed => failure
-      assert_equal "Expected '2' to `==` '3'", failure.message
-    end
-  end
-
-  must "custom_message_for_assert_equal" do
-    expected = "Hello, world!"
-    actual   = "Hello" + "World"
-
-    begin
-      assert_equal expected, actual, "Hello world requires punctuation!"
-    rescue TinyTest::Assertions::Failed => failure
-      assert_equal "Hello world requires punctuation!", failure.message
-    end
-  end
-end
 
 TinyTest.specify "running a single test" do
   must "report_result_of_single_passing_test" do
