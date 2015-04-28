d1bb7049fa5fa63760d2e776242eff506d69b630
Steve Tooke <steve@boxjump.co.uk>
2015-04-28 10:29:55 +0100

    Extract running tests test

--- 2_adventures/001/tooky/15/test/tiny_test_test.rb
+++ 2_adventures/001/tooky/15/test/tiny_test_test.rb
@@ -3,29 +3,7 @@ require 'tiny_test/autorun'
 
 
 
-TinyTest.specify "running a single test" do
-  must "report_result_of_single_passing_test" do
-    test_case = TinyTest::TestCase.define {}
-    result = TinyTest::Result.new
 
-    test_case.run(result)
-
-    assert_equal 1, result.run_count
-    assert_equal 1, result.passed_count
-    assert_equal 0, result.failed_count
-  end
-
-  must "report_result_of_single_failing_test" do
-    result = TinyTest::Result.new
-    test_case = TinyTest::TestCase.define { fail }
-
-    test_case.run(result)
-
-    assert_equal 1, result.run_count
-    assert_equal 0, result.passed_count
-    assert_equal 1, result.failed_count
-  end
-end
 
 TinyTest.specify "reporting results" do
   must "report_result_of_running_a_failing_and_passing_test" do
