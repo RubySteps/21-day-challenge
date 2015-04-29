4f4ef7c1d6534c8970f3b9c870f5045e4ef5c2d7
= <=>
2015-04-29 13:17:09 +0000

    Walking n steps is now a single function call.

--- 2_adventures/001/jbrains/17/langtons_ant_text_ui.rb
+++ 2_adventures/001/jbrains/17/langtons_ant_text_ui.rb
@@ -1,6 +1,10 @@
 require "./langtons_ant"
 
-walk = LangtonsAntWalk.new(ConsoleReportingWalkListener.new)
-20.times do
-  walk.take_a_step
+def walk(how_many_steps)
+  walk = LangtonsAntWalk.new(ConsoleReportingWalkListener.new)
+  how_many_steps.times do
+    walk.take_a_step
+  end
 end
+
+walk(20)
