1c43aed7a6f40f3587a7229bc7a3e39f99d43a82
J. B. Rainsberger <me@jbrains.ca>
2015-04-30 12:26:34 -0300

    Moved text UI stuff into the text UI source file, where it belonged.

--- 2_adventures/001/jbrains/18/langtons_ant.rb
+++ 2_adventures/001/jbrains/18/langtons_ant.rb
@@ -114,23 +114,6 @@ class WalkListener
   end
 end
 
-class ConsoleReportingWalkListener < WalkListener
-  def step_taken(number, ant)
-    puts (number == 0 ? "At the beginning of his walk..." : "After step #{number}")
-    describe_walk(ant)
-  end
-
-  def describe_walk(ant)
-    puts "Langton's ant is now at #{ant.location}, which is #{ant.grid.color_of(ant.location).to_s}"
-    puts "He is facing #{ant.facing}"
-    puts "He sees black squares at #{ant.grid.describe()}"
-  end
-
-  def color_flipped(location, color)
-    puts "The square at #{location} is changing to #{color}"
-  end
-end
-
 class LangtonsAntWalk 
   include Observable
 
