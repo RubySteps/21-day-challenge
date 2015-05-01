72b82f5b124ef09df27628a31675c073a619d2ee
Alex Wochna <awochna@email.arizona.edu>
2015-05-01 08:56:08 -0700

    Add more specs for completed task

--- lib/todotxt_rb/task.rb
+++ lib/todotxt_rb/task.rb
@@ -4,13 +4,21 @@ module TodotxtRb
     attr_accessor :added, :completed, :contexts, :done, :priority, :projects
 
     def initialize(string)
+      @original_string = string
       date_matcher = /\d{4}-\d{2}-\d{2}/
       context_matcher = /@\b\w+/
       project_matcher = /\+\b\w+/
       self.contexts = []
       self.projects = []
-      if date_matcher.match(string)
-        self.added = Date.parse(date_matcher.match(string)[0])
+      self.done = /^x\s/.match(string) ? true : false
+      date_match = date_matcher.match(string)
+      if date_match
+        if date_matcher.match(date_match.post_match)
+          self.completed = Date.parse(date_match[0])
+          self.added = Date.parse(date_matcher.match(date_match.post_match)[0])
+        else
+          self.added = Date.parse(date_match[0])
+        end
       else
         self.added = Date.today
       end
@@ -22,6 +30,10 @@ module TodotxtRb
       if project_match
         self.projects.push project_match[0]
       end
+    end
+
+    def to_str
+      @original_string
     end
 
     def done?
