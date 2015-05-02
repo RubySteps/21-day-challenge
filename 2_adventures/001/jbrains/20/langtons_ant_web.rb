f5e7cef694edd59bebbf770b3eb13d2912af5bc2
= <=>
2015-05-02 12:19:38 +0000

    We can signal that we're starting a new walk.

--- 2_adventures/001/jbrains/20/langtons_ant_web.rb
+++ 2_adventures/001/jbrains/20/langtons_ant_web.rb
@@ -1,6 +1,24 @@
 require "sinatra/base"
+require "tilt/erb"
+require "./langtons_ant"
+
+class InMemoryWalkListener < WalkListener
+  def step_taken(number, ant)
+    @number, @ant = number, ant
+  end
+
+  def color_flipped(location, color)
+    # Intentionally do nothing
+  end
+end
 
 class LangtonsAntWebApp < Sinatra::Application
+  get "/go" do
+    @walk_listener = InMemoryWalkListener.new
+    @walk = LangtonsAntWalk.new(@walk_listener)
+    erb "<h1>Starting a new walk</h1>"
+  end
+
   run! if app_file == $0
 end
 
