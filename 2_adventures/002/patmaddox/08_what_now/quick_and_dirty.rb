61c7f31cbca7ab61a50cbc64fdf86726ab9d141a
Pat Maddox <pat@rubysteps.com>
2015-05-12 10:16:35 -0600

    day 08

---
+++ 2_adventures/002/patmaddox/08_what_now/quick_and_dirty.rb
@@ -0,0 +1,30 @@
+def check_the_thing(path)
+  # fill this out tomorrow
+end
+
+def check_warmup
+  puts "checking warmups..."
+
+  path = File.expand_path("~/code/21-day-challenge/1_warmup/patmaddox")
+  files = Dir[path + "/**/*"].select {|f| f.include? 'README' }
+  directories = files.collect {|f| File.dirname(f) }
+  numbered_dirs = directories.collect {|d| d.split("/").last }
+  numbers = numbered_dirs.collect {|d| d.to_i }
+  puts numbers
+end
+
+def check_days_work
+  puts "checking days..."
+
+  path = File.expand_path("~/code/21-day-challenge/2_adventures/001/patmaddox")
+
+  files = Dir[path + "/**/*"].select {|f| f.include? 'README' }
+
+  directories = files.collect {|f| File.dirname(f) }
+  numbered_dirs = directories.collect {|d| d.split("/").last }
+  numbers = numbered_dirs.collect {|d| d.to_i }
+  puts numbers
+end
+
+check_warmup
+check_days_work
