1f073bf278d2b8b74e02bd4c203c4f0abf0eaca3
Noel Kelly <noel@noelkelly.biz>
2015-07-23 23:10:19 +1000

    add Day 17 README.md

--- 2_adventures/004/gnoll110/emerald/lib/emerald/application.rb
+++ 2_adventures/004/gnoll110/emerald/lib/emerald/application.rb
@@ -3,10 +3,14 @@ require 'emerald'
 module Emerald
   class Application
     def initialize
+      puts '>in Application init'
     end
  
   	def process (line)
-  	  line
+      puts 'in process'
+      puts '>>'+line
+      line = "110"
+  	  puts line
     end
   end
 end	
